8e17643dbe72979ba11cfc62549419227c69589f
Faraon4 <radu.darii130399@mail.ru>
2023-04-10 19:56:47 +0200

    Specification pattern to get one product having id

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Core.Entities;
 
@@ -11,11 +12,18 @@ namespace Core.Specifications
        // We need to create an empty contructor in the BaseSpecification class , to remove the error that appear when we create this class
 
 
-
+        // We use this ctor to get the list of products with types and brands
        public ProductsWithTypesAndBrandsSpecification()
        {
         AddInclude(x => x.ProductType);
         AddInclude(x => x.ProductBrand);
        }
+
+        // Contructor to get ONE PRODUCT with type and brand
+        public ProductsWithTypesAndBrandsSpecification(int id) : base(x => x.Id == id)
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }
     }
 }
