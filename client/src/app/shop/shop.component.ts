a446dcde5293e8e94512eeca5e4e63ca6da77259
Faraon4 <radu.darii130399@mail.ru>
2023-06-09 00:39:57 +0200

    passing brands and types to our shop from backend

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../shared/models/product';
 import { ShopService } from './shop.service';
+import { Brand } from '../shared/models/brand';
+import { Type } from '../shared/models/type';
 
 @Component({
   selector: 'app-shop',
@@ -9,6 +11,11 @@ import { ShopService } from './shop.service';
 })
 export class ShopComponent implements OnInit {
 products: Product[] = [];
+brands: Brand[] = []
+
+
+// Be carefully from where do we import the type , there are many places from where we can import it , we need exactly our interface from shared module
+types: Type[] = []
 
 constructor(private shopService: ShopService) {}
 
@@ -19,8 +26,28 @@ constructor(private shopService: ShopService) {}
 
   // This comments were deleted, please if you want to see more info where it was , look in the repo
   ngOnInit(): void {
-    this.shopService.getPRoducts().subscribe({
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProducts(){
+    this.shopService.getProducts().subscribe({
       next : response => this.products= response.data,
+      error: error =>  console.log(error)
+    })
+  }
+
+  getBrands(){
+    this.shopService.getBrands().subscribe({
+      next : response => this.brands= response,
+      error: error =>  console.log(error)
+    })
+  }
+
+  getTypes(){
+    this.shopService.getTypes().subscribe({
+      next : response => this.types= response,
       error: error =>  console.log(error)
     })
   }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Pagination } from '../shared/models/pagination';
 import { Product } from '../shared/models/product';
+import { Brand } from '../shared/models/brand';
+import { Type } from '../shared/models/type';
 
 @Injectable({
   providedIn: 'root' //  we can change here the module where we want to put
@@ -11,7 +13,16 @@ export class ShopService {
 
   constructor(private http: HttpClient) { }
 
-  getPRoducts(){
+  getProducts(){
     return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products?pageSize=50');
+  }
+
+  getBrands(){
+    return this.http.get<Brand[]>(this.baseUrl + 'products/brands')
+  }
+
+  getTypes(){
+    // Be carefully from where do we import the type , there are many places from where we can import it , we need exactly our interface from shared module
+    return this.http.get<Type[]>(this.baseUrl + 'products/types')
   }
 }
