0f1fc166d8d5185e47b49ad2524676418addcc13
REALEYESIT\radu.darii <radu.darii@realeyesit.com>
2023-07-11 11:41:22 +0200

    show the number of items in the basket icon

--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BasketService } from 'src/app/basket/basket.service';
+import { BasketItem } from 'src/app/shared/models/basket';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
+  // we are declaring it public , such way we'll be able to use it outside
+ constructor(public basketService: BasketService) {}
 
+
+ // this is a way to show all items in the basket -> even if we have 2 or more of the same kind
+
+ getCount(items: BasketItem[]) {
+  return items.reduce((sum, item) => sum + item.quantity, 0);
+ }
 }
