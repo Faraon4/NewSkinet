1d04e079d3ef405fb2a81af67f0b78d5281dc4e2
REALEYESIT\radu.darii <radu.darii@realeyesit.com>
2023-07-12 16:17:23 +0200

    Add the functionality of + and - and trassh button

--- client/src/app/basket/basket.component.ts
+++ client/src/app/basket/basket.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BasketService } from './basket.service';
+import { BasketItem } from '../shared/models/basket';
 
 @Component({
   selector: 'app-basket',
@@ -9,5 +10,13 @@ import { BasketService } from './basket.service';
 export class BasketComponent {
   // Adding the ctor and make the service to be public, such way we may use the service in the html
   constructor(public basketService: BasketService) {}
+
+  incrementQuantity(item: BasketItem) {
+    this.basketService.addItemToBasket(item);
+  }
+
+  removeItem(id: number, quantity: number) {
+    this.basketService.removeItemFromBasket(id, quantity);
+  }
 
 }
