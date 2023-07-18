2c05627f660a28e0169ae0c614a17ef078497519
REALEYESIT\radu.darii <radu.darii@realeyesit.com>
2023-07-18 17:18:00 +0200

    Add the Startup services for identity , some of the service will be configured after this like author and authent.

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Entities.Identity;
 using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions
@@ -14,6 +16,16 @@ namespace API.Extensions
             services.AddDbContext<AppIdentityDbContext>(opt => {
                 opt.UseSqlite(configuration.GetConnectionString("IdentityConnection"));
             });
+            services.AddIdentityCore<AppUser> (opt => 
+            {
+                // we can add identity options here
+            })
+            .AddEntityFrameworkStores<AppIdentityDbContext>()
+            .AddSignInManager<SignInManager<AppUser>>();
+
+            // It is important to add Authentication and Authorization in this order, because on ly like this we check who the pers is and then what he/she can do
+            services.AddAuthentication();
+            services.AddAuthorization();
 
             return services;
         }
