95b60653da991825d4a11b2424dc90017e4f81f3
REALEYESIT\radu.darii <radu.darii@realeyesit.com>
2023-07-21 14:58:56 +0200

    setup identoty to use token

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Core.Entities.Identity;
 using Infrastructure.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Extensions
 {
@@ -24,7 +27,23 @@ namespace API.Extensions
             .AddSignInManager<SignInManager<AppUser>>();
 
             // It is important to add Authentication and Authorization in this order, because on ly like this we check who the pers is and then what he/she can do
-            services.AddAuthentication();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options => 
+                    {
+                        // We are writing what we want to do to validate this token
+                        options.TokenValidationParameters = new TokenValidationParameters // this TokenValidationParameters -> is very important from where to import
+                        {
+                            // This is first what it is needed to be validated
+                            ValidateIssuerSigningKey = true,
+                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Token:Key"])),
+                            ValidIssuer = configuration["Token:Issuer"],
+                            ValidateIssuer = true
+
+                        };
+                    });
+
+
+
             services.AddAuthorization();
 
             return services;
