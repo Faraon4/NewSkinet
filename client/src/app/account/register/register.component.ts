c308ee285b5e85add70186b105a03294baf71368
Faraon4 <radu.darii130399@mail.ru>
2023-10-19 00:01:07 +0200

    creating the async validation If the email for registration is already used --> that it is not possible to use it more --> this feaure is implemented in this files

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { AbstractControl, AsyncValidatorFn, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AccountService } from '../account.service';
+import { debounce, debounceTime, finalize, map, switchMap, take } from 'rxjs';
 
 @Component({
   selector: 'app-register',
@@ -17,7 +18,7 @@ export class RegisterComponent {
 
     registerForm = this.fb.group({
       displayName: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
+      email: ['', [Validators.required, Validators.email], [this.validateEmailNotTaken()]],
       password: ['', [Validators.required, Validators.pattern(this.complexPassword)]]
     })
 
@@ -26,6 +27,24 @@ export class RegisterComponent {
         next: () => this.router.navigateByUrl('/shop'),
         error: error => this.errors = error.errors
       })
+    }
+
+    validateEmailNotTaken(): AsyncValidatorFn {
+      return (control: AbstractControl) => {
+        // we are using it for not calling all the time the API when we introduce new char to the email field
+        //take(1) --> takes the last example that we typed take will send it to the URL api
+        // switchMap --> send the last example to the URL api backend to perform the necesary check
+        return control.valueChanges.pipe(
+          debounceTime(1000),
+          take(1),
+          switchMap(() => {
+            return this.accountService.checkEmailExists(control.value).pipe(
+              map( result => result ? {emailExists: true} : null),
+              finalize(() => control.markAsTouched())
+            )
+          })
+        )
+      }
     }
 
 }
