b88b9d8f9a18bab32056047740d132aee370b938
Faraon4 <radu.darii130399@mail.ru>
2023-10-19 00:30:19 +0200

    introduce the auth guard

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AccountService } from '../account.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -14,15 +14,17 @@ loginForm = new FormGroup( {
   email: new FormControl('', [Validators.required,Validators.email]),
   password: new FormControl('', Validators.required),
 })
+returnUrl: string;
 
-
-constructor(private accountService: AccountService, private router: Router) {}
+constructor(private accountService: AccountService, private router: Router, private activatedRoute: ActivatedRoute) {
+this.returnUrl =  this.activatedRoute.snapshot.queryParams['returnUrl'] || '/shop' // add the check because , if we try to access the basket without being login , we have a new url generated, which we do not need
+}
 
 // this will be used in the template to say when press the submition button and will get login
 // in this case , after succeful login , it will redirect to the shop page
 onSubmit() {
   this.accountService.login(this.loginForm.value).subscribe({
-    next: user=> this.router.navigateByUrl('/shop')
+    next: user=> this.router.navigateByUrl(this.returnUrl)
   });
 }
 
