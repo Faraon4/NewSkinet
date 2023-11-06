8c0ecf5ba82ac1d3a657aedebb6a174220e8146d
Faraon4 <radu.darii130399@mail.ru>
2023-11-07 00:34:27 +0100

    Create the Order class (entity)

---
+++ Core/Entities/OrderAggregate/Order.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Entities.OrderAggregate
+{
+    public class Order : BaseEntity
+    {
+        // for entityFramework and table creation
+        public Order()
+        {
+        }
+
+        public Order(IReadOnlyList<OrderItem> orderItems,string buyerEmail , Address shipToAddress, 
+            DeliveryMethod deliveryMethod, decimal subtotal)
+        {
+            BuyerEmail = buyerEmail;
+            ShipToAddress = shipToAddress;
+            DeliveryMethod = deliveryMethod;
+            OrderItems = orderItems;
+            Subtotal = subtotal;
+        }
+
+        public string BuyerEmail  { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+        public Address ShipToAddress { get; set; }
+        public DeliveryMethod DeliveryMethod { get; set; }
+        public IReadOnlyList<OrderItem> OrderItems { get; set; }
+        public decimal Subtotal { get; set; }
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+        public string PaymenyIntentId { get; set; } // will be used for stripe payment
+        public decimal GetTotal() // Get before the name it is used for automaper to execute method inside {} and map to a property called Total
+        {
+            return Subtotal + DeliveryMethod.Price;
+        }
+    }
+}
