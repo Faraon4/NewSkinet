abf719fc19418a32352c79b7ce8a894601885ee0
Faraon4 <radu.darii130399@mail.ru>
2023-11-07 01:09:42 +0100

    Create the new migration --> OrderEntityAdded class file we can see that it created the owned method from the helping classes that we created at the beginning of this section

--- Infrastructure/Data/Config/OrderConfiguration.cs
+++ Infrastructure/Data/Config/OrderConfiguration.cs
@@ -17,6 +17,7 @@ namespace Infrastructure.Data.Config
             {
                 a.WithOwner();
             });
+            builder.Navigation(a => a.ShipToAddress).IsRequired();
             builder.Property(s => s.Status)
                    .HasConversion(
                     o => o.ToString(),
