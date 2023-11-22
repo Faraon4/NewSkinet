97c767579db11fa51574786bc5ab67b6734c07a5
Faraon4 <radu.darii130399@mail.ru>
2023-11-23 00:49:59 +0100

    Create the first version of the address for the checkout form

--- client/src/app/checkout/checkout.component.ts
+++ client/src/app/checkout/checkout.component.ts
@@ -10,11 +10,14 @@ export class CheckoutComponent {
 
     constructor(private fb: FormBuilder) {}
 
+    // it will be used to track the checkout at every level
+    // every new group will be used in the necesary html modules to check and to purside
     checkoutForm = this.fb.group({
       addressForm: this.fb.group({
         firstName:['', Validators.required],
         lastName:['', Validators.required],
         street:['', Validators.required],
+        city:['', Validators.required],
         state:['', Validators.required],
         zipcode:['', Validators.required]
       }),
