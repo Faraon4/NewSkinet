0da82039008fdddfa53ca3a8e22ae64a258b8cb5
Faraon4 <radu.darii130399@mail.ru>
2023-11-26 21:20:26 +0100

    Add the possibility to save the address and to update the address

--- client/src/app/checkout/checkout-address/checkout-address.component.ts
+++ client/src/app/checkout/checkout-address/checkout-address.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from 'src/app/account/account.service';
 
 @Component({
   selector: 'app-checkout-address',
@@ -9,4 +11,19 @@ import { FormGroup } from '@angular/forms';
 export class CheckoutAddressComponent {
 @Input() checkoutForm?: FormGroup; // use it as inout such way that we are able to send to checkout.cmp.ts
 
+
+constructor(private accountService: AccountService, private toastr: ToastrService) {}
+
+
+
+// We add the ability to save the upadted data after changing the data in address form
+// reset() method --> need to have in side () this statemenet because otherwise , the form is reseted and no information theer is written
+saveUserAddress(){
+  this.accountService.updateUserAddress(this.checkoutForm?.get('addressForm')?.value).subscribe({
+    next: () => {
+      this.toastr.success('Address saved');
+      this.checkoutForm?.get('addressForm')?.reset(this.checkoutForm?.get('addressForm')?.value);
+  }
+  })
+}
 }
