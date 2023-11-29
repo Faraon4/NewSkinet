6418be29856b666e835a2f22d7d7e176b7633332
Faraon4 <radu.darii130399@mail.ru>
2023-11-29 01:24:41 +0100

    Create the controller for the payments

--- API/Dtos/CustomerBasketDto.cs
+++ API/Dtos/CustomerBasketDto.cs
@@ -11,5 +11,12 @@ namespace API.Dtos
         [Required]
         public string Id { get; set; } 
         public List<BasketItemDto> Items { get; set; } = new List<BasketItemDto>();
+
+        // This 3 properties we copy from the CustomerBasket class itself
+        public int? DeliveryMethodId { get; set; }
+
+        public string ClientSecret { get; set; } 
+        public string PaymentIntentId { get; set; } 
+   
     }
 }
