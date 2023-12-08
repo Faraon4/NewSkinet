33eb84ab457f116e5dbf51c7359811ca1a8a2fb8
Faraon4 <radu.darii130399@mail.ru>
2023-12-08 17:49:56 +0100

    Correct in html the spelling
    Make the user to input the data for the card using stripe elements

--- client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { BasketService } from 'src/app/basket/basket.service';
 import { CheckoutService } from '../checkout.service';
@@ -6,19 +6,48 @@ import { ToastrService } from 'ngx-toastr';
 import { Basket } from 'src/app/shared/models/basket';
 import { Address } from 'src/app/shared/models/user';
 import { NavigationExtras, Router } from '@angular/router';
+import { Stripe, StripeCardCvcElement, StripeCardExpiryElement, StripeCardNumberElement, loadStripe } from '@stripe/stripe-js';
 
 @Component({
   selector: 'app-checkout-payment',
   templateUrl: './checkout-payment.component.html',
   styleUrls: ['./checkout-payment.component.scss']
 })
-export class CheckoutPaymentComponent {
+export class CheckoutPaymentComponent implements OnInit{
 @Input() checkoutForm? : FormGroup;
+@ViewChild('cardNumber') cardNumberElement?: ElementRef;
+@ViewChild('cardExpiry') cardExpiryElement?: ElementRef;
+@ViewChild('cardCvc') cardCvcElement?: ElementRef;
+stripe: Stripe | null = null;
+cardNumber?: StripeCardNumberElement;
+cardExpiry?: StripeCardExpiryElement;
+cardCvc?: StripeCardCvcElement;
+
 
 constructor(private basketService: BasketService, 
             private checkoutService: CheckoutService, 
             private toastr: ToastrService,
             private router: Router){}
+
+
+
+  ngOnInit(): void {
+    // we get method from stripeJS ; and between () is the publishableKey
+    loadStripe("pk_test_51OHaHOFoa3I1sw1FIYpfBXaUJWCBRzrOpkO9kxO6p0nGVwmERqjl1HnUh8dr59GjCXpJmG2FgQfHNfZ1rFMfZLOI000KbOwCbz").then(stripe => {
+      this.stripe = stripe;
+      const elements = stripe?.elements();
+      if (elements) {
+        this.cardNumber = elements.create('cardNumber');
+        this.cardNumber.mount(this.cardNumberElement?.nativeElement);
+
+        this.cardExpiry = elements.create('cardExpiry');
+        this.cardExpiry.mount(this.cardExpiryElement?.nativeElement);
+
+        this.cardCvc = elements.create('cardCvc');
+        this.cardCvc.mount(this.cardCvcElement?.nativeElement);
+      }
+    })
+  }
 
 
   submitOrder() {
