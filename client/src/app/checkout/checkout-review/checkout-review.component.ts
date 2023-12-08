2aac39acb29f749b8756b601b09999e1770498be
Faraon4 <radu.darii130399@mail.ru>
2023-12-08 18:05:44 +0100

    Using the appStepper to create the payment intent and from review to go to the payment

--- client/src/app/checkout/checkout-review/checkout-review.component.ts
+++ client/src/app/checkout/checkout-review/checkout-review.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { CdkStepper } from '@angular/cdk/stepper';
+import { Component, Input } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { BasketService } from 'src/app/basket/basket.service';
 
@@ -8,11 +9,15 @@ import { BasketService } from 'src/app/basket/basket.service';
   styleUrls: ['./checkout-review.component.scss']
 })
 export class CheckoutReviewComponent {
+  @Input() appStepper?: CdkStepper;
   constructor(private basketService: BasketService, private toastr: ToastrService) {}
 
   createPaymentIntent(){
     this.basketService.createPaymentIntent().subscribe({
-      next: () => this.toastr.success('Payment created with success'),
+      next: () => {
+        this.toastr.success('Payment created with success');
+        this.appStepper?.next();
+    },
       error: error => this.toastr.error(error.message)
     })
   }
